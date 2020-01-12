23a3301b86a6caadc1cc5e28e30a94369d9c5082
Pavel Shabalin <aisamiery@gmail.com>
2020-01-12 19:22:37 +0300

    homework lesson 6 nodejs

---
+++ lesson-6/request.js
@@ -0,0 +1,35 @@
+const request = require('request');
+const util = require('util');
+
+const TYPE_ASYNC = 1;
+const TYPE_SYNC = 0;
+
+const async = util.promisify(request);
+
+const count = process.argv[2] || 1;
+const type = Number(process.argv[3]) === TYPE_ASYNC ? TYPE_ASYNC : TYPE_SYNC;
+
+async function get(count = 0) {
+    const response = await async('http://localhost:8080');
+
+    console.log(response.body);
+
+    count--;
+
+    if (count > 0) {
+        return get(count);
+    }
+
+    return response;
+}
+
+if (type === TYPE_SYNC) {
+    get(count)
+} else {
+    for(let i = 0; i < count; i++) {
+        request('http://localhost:8080', (err, res, body) => {
+            console.log(`request ${i} end`);
+            console.log(body);
+        });
+    }
+}
